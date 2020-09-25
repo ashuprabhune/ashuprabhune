378cd92a5b8b8ed4175a9d23f8cdb97a645216fa
Ashish Prabhune <ashuprabhune@gmail.com>
2020-09-24 21:06:00 -0400

    Updates

---
+++ precache-manifest.5be9fe6af14db6c5bf4471dfc2cd548e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1818296ca77055cbf14303fd3185d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17f2c528604adb3300dd",
+    "url": "/static/css/main.6333ecd6.chunk.css"
+  },
+  {
+    "revision": "d7635f02d7f4f22ded07",
+    "url": "/static/js/2.6694f8d1.chunk.js"
+  },
+  {
+    "revision": "7d763b78697d0d0900e3a3ce04ca0d45",
+    "url": "/static/js/2.6694f8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f2c528604adb3300dd",
+    "url": "/static/js/main.3acafed1.chunk.js"
+  },
+  {
+    "revision": "11d268e65cc31bc32ff5",
+    "url": "/static/js/runtime-main.e20c7041.js"
+  }
+]);
